fb8e5113bb5d820e72ace0eebca608af4c7b979b
vijayvir <virvijay37@gmail.com>
2019-08-02 10:42:43 +0530

    g
    g

---
+++ LeoChatXmpp/LeoChatXmpp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  LeoChatXmpp
+//
+//  Created by tecH on 18/06/19.
+//  Copyright © 2019 vijayvir Singh. All rights reserved.
+//
+
+//https://www.erlang-solutions.com/blog/build-a-complete-ios-messaging-app-using-xmppframework-tutorial-part-1.html
+//https://github.com/inaka/ios-xmpp-sample
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
